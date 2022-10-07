d637a8ab402cbaa0b8a177b4b11e13df49e6213c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-10-07 10:02:39 -0400

    url spec

---
+++ cypress/e2e/url-spec.js
@@ -0,0 +1,42 @@
+/// <reference types="cypress" />
+import { resetDatabase } from './utils'
+
+import spok from 'cy-spok'
+import todos from '../fixtures/example.json'
+
+describe('Todo API', () => {
+  beforeEach(() => {
+    resetDatabase(todos)
+  })
+
+  it('adds a todo', () => {
+    cy.visit(
+      '/?type=completed&n=4&start_id=201&_cache_bust=' + Cypress._.random(1e6),
+    )
+    cy.location('search')
+      .should('include', 'type=completed&n=4&start_id=201&_cache_bust=')
+      .then((s) => new URLSearchParams(s))
+      .invoke('entries')
+      .then(Array.from)
+      .then(Cypress._.fromPairs)
+      .then(console.log)
+      .then((o) => {
+        Cypress._.update(o, 'n', Number)
+        Cypress._.update(o, 'start_id', Number)
+      })
+      .should(
+        spok({
+          type: (s) => ['active', 'completed'].includes(s),
+          n: todos.length,
+          start_id: spok.number,
+        }),
+      )
+      .and(
+        spok({
+          _cache_bust: spok.string,
+        }),
+      )
+      .its('_cache_bust')
+      .should('have.length.greaterThan', 5)
+  })
+})
